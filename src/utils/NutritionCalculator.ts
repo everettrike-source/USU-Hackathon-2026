273f6c2af40e170c105294fa7705676f5b8439dd
Kyle <chacopineapple@gmail.com>
2026-02-27 22:20:06 -0700

    Finished calorie calculator

--- src/utils/NutritionCalculator.ts
+++ src/utils/NutritionCalculator.ts
@@ -3,6 +3,7 @@ export type UserInformation = {
     weight: number; 
     gender: 'male' | 'female'; 
     activityLevel: number;
+    activityIntensity: number;
     heightFoot: number;
     heightInches: number;
 }
@@ -10,7 +11,6 @@ export type UserInformation = {
 export type ExtraInformation = {
     targetWeight: number;
     targetMonth: number;
-    bmr: number;
 }
 
 export function calculateBaseCalories(user: UserInformation): number {
@@ -27,18 +27,28 @@ export function calculateBaseCalories(user: UserInformation): number {
     }
 
      BMR *= 1.2;
-     BMR += exerciseMin * (5.0 * 3.5 * weightkg) / 200;
-    user.bmr = BMR;
+     if(exerciseMin > 0){
+     BMR += (user.activityIntensity * 3.5 * weightkg / 200) * exerciseMin;
+     }
     return Math.round(BMR);
 }
 //if under or over max or min alert them saying by that date that this is max/min weight healthly possible to reach.
 export function bulkCutCalories(user: UserInformation, extra: ExtraInformation): number {
+    const maintenance = calculateBaseCalories(user);
     const weightDifference = extra.targetWeight - user.weight;
     const daysToTarget = 30.5 * extra.targetMonth;  
     const totalCalorieShift = weightDifference * 3500;
-    const finalTarget = extra.bmr + (totalCalorieShift/daysToTarget);
+    const finalTarget = maintenance + (totalCalorieShift/daysToTarget);
+    if(finalTarget < 1200) {
+        console.warn(`Your target calorie intake is very low. It's recommended to not go below 1200 calories per day for health reasons.`);
+    }
+    if(finalTarget > 5000) {
+        console.warn(`Your target calorie intake is very high. It's recommended to not go above 5000 calories per day for health reasons.`);
+    }
     return Math.round(Math.max(1200, Math.min(finalTarget, 5000)));
 }
-const Everett: UserInformation = {age:18,weight: 160, gender:'male',activityLevel: 14,heightFoot:6,heightInches:1};
-const myCalories = calculateBaseCalories(Everett);
-console.log(myCalories);
+const Everett: UserInformation = {age:18,weight: 160, gender:'male',activityLevel: 5,activityIntensity: 8,heightFoot:6,heightInches:1};
+const EverettExtra: ExtraInformation = {targetWeight: 200, targetMonth: 12};
+const myBulkCutCalories = bulkCutCalories(Everett, EverettExtra);
+console.log(calculateBaseCalories(Everett));
+console.log(myBulkCutCalories);
